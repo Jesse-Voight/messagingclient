932006e8a26f878aa6c36ae2fd5572345e8ee227
Jesse Voight <24051java1@gmail.com>
2014-11-09 15:53:31 +1300

    Changed the way it picks up messages, still working things out

--- src/wdhb/nmc/WDHBNMC.java
+++ src/wdhb/nmc/WDHBNMC.java
@@ -1,13 +1,14 @@
-
 package wdhb.nmc;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,33 +16,36 @@ import java.util.logging.Logger;
  * @description Network Message Client
  */
 public class WDHBNMC {
-    private static int networkSocketNumber = 1234;
+
+    private static int networkSocketNumber = 6789;
+    private static int programStatus;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        if(args.length > 1){
-        if(!args[0].equals("")){
-            try{
-                networkSocketNumber = Integer.valueOf(args[0]);
+        programStatus = 0;
+        while (programStatus == 0) {
+            try {
+                ServerSocket listenSocket = new ServerSocket(networkSocketNumber);
+                Socket liveSocket = listenSocket.accept();
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(liveSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(liveSocket.getOutputStream(), true);
+                out.write("Message Recieved");
+                out.flush();
+                String message = inReader.readLine();
+                out.close();
+                inReader.close();
+                liveSocket.close();
+                listenSocket.close();
+                JOptionPane.showMessageDialog(null, message);
+                System.out.println(message);
+                //liveSocket.setKeepAlive(true);
+            } catch (IOException ex) {
+                System.err.println("Could not create socket for port: " + String.valueOf(networkSocketNumber));
+                Logger.getLogger(WDHBNMC.class.getName()).log(Level.SEVERE, null, ex);
             }
-            catch(NumberFormatException e){
-                System.err.println(e);
-            }
-        }
-        }
-        try {
-            Socket listenSocket = new Socket("wsc852",networkSocketNumber);
-            BufferedReader inReader = new BufferedReader(new InputStreamReader(listenSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(listenSocket.getOutputStream(), true);
-            out.write("Test From Colient");
-            out.flush();
-            System.out.println(inReader.readLine());
-        } 
-        catch (IOException ex) {
-            System.err.println("Could not create socket for port: "+String.valueOf(networkSocketNumber));
-            Logger.getLogger(WDHBNMC.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
